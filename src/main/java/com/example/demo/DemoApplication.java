f039dbc2c86c0de9555bd4d50eb2cc6f8104c7a8
pokev25 <pokev25@gmail.com>
2020-04-29 18:32:25 +0900

    테스트 설정 추가

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,6 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-
-import com.example.demo.util.BeanUtil;
-import com.example.demo.util.MySingletonBean;
 
 @SpringBootApplication
 public class DemoApplication {
